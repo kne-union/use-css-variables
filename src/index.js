d3459a5a66f3e69ea064b0bcef7fdc8237db8050
林志鹏 <zhipenglin@knx.com.cn>
2020-09-01 16:03:40 +0800

    修改了出发时机

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useMemo, useCallback, useRef} from 'react';
 import 'construct-style-sheets-polyfill';
 
 export default (obj) => {
     const sheetRef = useRef(new CSSStyleSheet());
 
-    const removeCurrentSheet = () => {
+    const removeCurrentSheet = useCallback(() => {
         const ls = document.adoptedStyleSheets.slice(0);
         const index = ls.indexOf(sheetRef.current);
         if (index > -1) {
             ls.splice(index, 1);
         }
         document.adoptedStyleSheets = ls;
-    };
+    }, []);
 
-    useEffect(() => {
+    useMemo(() => {
         sheetRef.current.replaceSync(`:root{ ${Object.keys(obj).map((name) => `--${name}:${obj[name]};`).join('')} }`);
         removeCurrentSheet();
         document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheetRef.current];
+    }, [obj, removeCurrentSheet]);
+
+    useEffect(() => {
         return () => {
             removeCurrentSheet();
         };
-    }, [obj]);
+    }, [removeCurrentSheet]);
 
     return removeCurrentSheet;
 };
